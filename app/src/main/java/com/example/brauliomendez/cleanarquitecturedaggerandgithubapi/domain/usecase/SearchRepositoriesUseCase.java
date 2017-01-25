de845dd10fcbb020cb3e23e696975d5d4d62d1ed
leonel Mendez <Leo Mendez>
2017-01-24 23:33:07 -0600

    UPDATE: Added repository dependency into usecase

--- app/src/main/java/com/example/brauliomendez/cleanarquitecturedaggerandgithubapi/domain/usecase/SearchRepositoriesUseCase.java
+++ app/src/main/java/com/example/brauliomendez/cleanarquitecturedaggerandgithubapi/domain/usecase/SearchRepositoriesUseCase.java
@@ -1,9 +1,25 @@
 package com.example.brauliomendez.cleanarquitecturedaggerandgithubapi.domain.usecase;
 
+
+import com.example.brauliomendez.cleanarquitecturedaggerandgithubapi.domain.entity.SearchResponse;
+import com.example.brauliomendez.cleanarquitecturedaggerandgithubapi.domain.repository.GithubSearchRepository;
+
+import io.reactivex.Observable;
+
 /**
  * Created by Leo on 24/01/17.
  */
 
 public class SearchRepositoriesUseCase {
 
+  private GithubSearchRepository githubSearchRepository;
+
+  public SearchRepositoriesUseCase(
+      GithubSearchRepository githubSearchRepository) {
+    this.githubSearchRepository = githubSearchRepository;
+  }
+
+  public Observable<SearchResponse> getGithubRepositories(String username){
+    return githubSearchRepository.searchGithubRepositories(username);
+  }
 }
